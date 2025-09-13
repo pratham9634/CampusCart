8173b3c4d77f16923dd9fc914b3207b9b72dd517
pratham9634 <prathampetwal100@gmail.com>
2025-09-13 23:38:39 +0530

    initialize clerk

---
+++ src/app/(auth)/sign-in/[[...sigin-in]]/page.jsx
@@ -0,0 +1,9 @@
+import { SignIn } from '@clerk/nextjs';
+
+export default function SigninInPage() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center p-10">
+      <SignIn />
+    </main>
+  );
+}
